56827781b16e1136e4ad854a34e3ef7fae382c9d
drb101005 <drb101005@gmail.com>
2026-06-28 12:39:33 +0530

    .

--- 01/main.py
+++ 01/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from typing import Optional
 
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 app = FastAPI()
-
+class data(BaseModel):
+    Title : str
+    Name : str
+    published : bool = True
+    rating : Optional[int] = None 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/Posts")
+async def get_posts():
+    return {"Data" : "27 gb"}
+
+@app.post("/createpost")
+def create_post(New_data : data):
+    print(New_data.published)
+    print(New_data.rating)
+    return {"data": "new post"}
